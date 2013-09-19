fb2e235740c26905d743fdb5d13751f04deb20f3
compwron <wordymusic@gmail.com>
2013-09-18 22:20:21 -0500

    adding readme with problem statement

---
+++ README.md
@@ -0,0 +1,5 @@
+This code does not work yet.
+
+I am interested in building "timed ignore" i.e. @Ignore(new DateTime("2013-09-20") and have the @Ignore no longer ignore after the given date.
+
+Using example from http://techo-ecco.com/blog/spring-custom-annotations/
--- src/ClasspathScanner.java
+++ src/ClasspathScanner.java
@@ -6,10 +6,10 @@ import java.util.Set;
 public class ClasspathScanner {
     public void scan() {
         final Reflections reflections = new Reflections("org.projectx", new TypeAnnotationsScanner());
-        Set<Class<?>> theFoos = reflections.getTypesAnnotatedWith(TimedIgnore.class);
-        for (final Class<?> theFoo : theFoos) {
-            final TimedIgnore annotation = theFoo.getAnnotation(TimedIgnore.class);
-            System.out.println("This foo " + theFoo.getClass().getName() + "has expires of: "+annotation.expires());
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(TimedIgnore.class);
+        for (final Class<?> currentClass : classes) {
+            final TimedIgnore timedIgnoreAnnotation = currentClass.getAnnotation(TimedIgnore.class);
+            System.out.println("This foo " + currentClass.getClass().getName() + "has expires of: " + timedIgnoreAnnotation.expires());
         }
     }
 

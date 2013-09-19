8f94b005a534ad440ba7f6ac3e0aaa95337673cc
compwron <wordymusic@gmail.com>
2013-09-18 23:32:16 -0500

    renaming utility method

--- test/TimedIgnoreTest.java
+++ test/TimedIgnoreTest.java
@@ -8,17 +8,17 @@ public class TimedIgnoreTest {
 
     @Test
     public void shouldRunBecauseAssumptionHasTimedOut() {
-        runOnDateBecause("2012-01-01", "todays date is past 2012");
-        fail("See message from runOnDateBecause");
+        runAfterDateBecause("2012-01-01", "todays date is past 2012");
+        fail("See message from runAfterDateBecause");
     }
 
     @Test
     public void shouldNotRunBefore2020BecauseAssumptionHasNotTimedOut() {
-        runOnDateBecause("2020-01-01", "today's date is past 2020-01-01");
-        fail("See message from runOnDateBecause");
+        runAfterDateBecause("2020-01-01", "today's date is past 2020-01-01");
+        fail("See message from runAfterDateBecause");
     }
 
-    private void runOnDateBecause(String date, String reasonToStartRunning) {
+    private void runAfterDateBecause(String date, String reasonToStartRunning) {
         if (new DateTime().isAfter(new DateTime(date))) { // May have to replace this with non-Joda date parsing in older codebases
 //            Logger.log("Running test because " + reasonToStartRunning);
             System.out.println("Running test because " + reasonToStartRunning);

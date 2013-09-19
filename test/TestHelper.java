b63e602303a649fe9d29a9e4b2bcf1b703d1ca56
compwron <wordymusic@gmail.com>
2013-09-18 23:49:14 -0500

    Adding more informative logging in runAfterDateBecause

--- test/TestHelper.java
+++ test/TestHelper.java
@@ -1,14 +1,33 @@
-import org.joda.time.DateTime;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.junit.Assume.assumeTrue;
 
 public class TestHelper {
-    static void runAfterDateBecause(String date, String reasonToStartRunning) {
-        if (new DateTime().isAfter(new DateTime(date))) { // May have to replace this with non-Joda date parsing in older codebases
+
+//    Remove usage of this method if the date today is after the runAfter date
+    static void runAfterDateBecause(String runAfter, String reasonToStartRunning) {
+        if (givenDateIsAfterCurrentDate(runAfter)) { // May have to replace this with non-Joda date parsing in older codebases
 //            Logger.log("Running test because " + reasonToStartRunning);
-            System.out.println("Running test because " + reasonToStartRunning);
+            System.out.println("Running test because " + reasonToStartRunning + " ...If this test passes, please delete the runAfterDateBecause usage");
         } else {
+            System.out.println("Skipping test because run date is in the future: " + runAfter);
             assumeTrue(false);
         }
+    }
+
+    private static boolean givenDateIsAfterCurrentDate(String date) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date givenDate = null;
+        try {
+            givenDate = format.parse(date);
+        } catch (ParseException e) {
+            System.out.println("Could not parse date, returning true so that test will run");
+            return true;
+        }
+        return new Date().after(givenDate);
+
+//        return new DateTime().isAfter(new DateTime(date)); // JodaTime
     }
 }
--- test/TimedIgnoreTest.java
+++ test/TimedIgnoreTest.java
@@ -1,19 +1,25 @@
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static junit.framework.TestCase.assertTrue;
 
 public class TimedIgnoreTest {
 
     @Test
     public void shouldRunBecauseAssumptionHasTimedOut() {
-        TestHelper.runAfterDateBecause("2012-01-01", "todays date is past 2012");
-        fail("See message from runAfterDateBecause");
+        TestHelper.runAfterDateBecause("2012-01-01", "the date today is past 2012");
+        assertTrue(true);
     }
 
     @Test
     public void shouldNotRunBefore2020BecauseAssumptionHasNotTimedOut() {
-        TestHelper.runAfterDateBecause("2020-01-01", "today's date is past 2020-01-01");
-        fail("See message from runAfterDateBecause");
+        TestHelper.runAfterDateBecause("2020-01-01", "the date today is past 2020-01-01");
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldRunBecauseInvalidDateIsPassed() {
+        TestHelper.runAfterDateBecause("invalid date", "the date today is past given date (which is invalid)");
+        assertTrue(true);
     }
 
 
